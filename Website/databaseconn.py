af202de45f5951817d2dd34dd82d9ff83a168e05
Jacques741 <88324973+Jacques741@users.noreply.github.com>
2023-07-30 12:53:27 +0200

    Update databaseconn.py

--- Website/databaseconn.py
+++ Website/databaseconn.py
@@ -9,3 +9,27 @@ db = mysql.connector.connect(
     database="sql6634220",
     connect_timeout=30
 )
+
+#Using Flask MySQLDB to form a connection to DB, works and tested with version 3.7+.
+#'pip install flas-mysqldb', for connection to work.
+
+from Flas import Flask
+from flask_mysqldb import MySQL
+
+#needed
+app = Flask(__name__)
+app.config["MSQL_USER"] = "user"
+app.config["MSQL_PASSWORD"] = "password"
+app.config["MSQL_DB"] = "database"
+
+mysql = MySQL(app)
+
+@app.route("/")
+def user():
+    cur = mysql.connection.cursor()
+    cur.execute("""SELECT user, host FROM mysql.user""")
+    rv = cur.fetchhall()
+    return str(rv)
+
+if __name__ == '__main__':
+    app.run(debug=True)
